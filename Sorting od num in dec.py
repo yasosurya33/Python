ddc7f873faec69d4195f5d3376342921dc1e6f71
Surya S <51106976+yasosurya33@users.noreply.github.com>
2019-08-13 23:35:29 +0530

    Update Sorting od num in dec.py

--- Sorting od num in dec.py
+++ Sorting od num in dec.py
@@ -1,6 +1,9 @@
 num=int(input())
-dub=input().split()[:num]
-dub.sort()
-dub.reverse()
+dub=map(int,input().split()[:num])
+dub1=[]
 for i in dub:
+    dub1.append(int(i))
+dub1.sort()
+dub1.reverse()
+for i in dub1:
     print(i)
